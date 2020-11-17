3deac4dc5ae1970b39fce07b60936aa618d91a3d
abdullah <abdullah34alrafi@gmail.com>
2020-11-17 22:35:26 +0600

    Payload model added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Payload(models.Model):
+    passphrase = models.CharField()
+    message = models.CharField()
+
+   def __str__(self):
+        return self.passphrase
+    
